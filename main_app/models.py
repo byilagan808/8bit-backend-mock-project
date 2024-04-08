6a00dacc962b5f321781cfca164146dffb06a8ee
byilagan808 <b808ilagan@gmail.com>
2024-04-08 01:35:57 -1000

    create and register image model

--- main_app/models.py
+++ main_app/models.py
@@ -9,3 +9,13 @@ class File(models.Model):
     def __str__(self): 
         return self.filename
 
+class MiscImage(models.Model): 
+    name = models.CharField(max_length=100) 
+    image = models.ImageField(upload_to='main_app.File/bytes/filename/mimetype', null=True,help_text="Please compress image and convert type to webp before uploading. https://imagecompressor.com/, https://cloudconvert.com/webp-converter") 
+
+    def __str__(self): 
+        return self.name
+
+    def delete(self, *args, **kwargs): 
+        super(MiscImage, self).delete(*args, **kwargs)
+        File.objects.filter(filename = self.image.name).delete()
