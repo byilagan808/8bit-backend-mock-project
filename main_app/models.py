b4097733925dc711077da397b240ed7ac59f6732
byilagan808 <b808ilagan@gmail.com>
2024-04-08 01:28:21 -1000

    file model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class File(models.Model): 
+    bytes = models.TextField() 
+    filename = models.CharField(max_length=255) 
+    mimetype = models.CharField(max_length=50) 
+
+    def __str__(self): 
+        return self.filename
+
