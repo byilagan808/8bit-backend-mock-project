f4609ff24b9a4130100d82b57da981ce89e90fbd
byilagan808 <b808ilagan@gmail.com>
2024-04-08 13:05:00 -1000

    looping through model data

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    shows = TvShow.objects.prefetch_related("images").all()
+
+    return render(request, "index.html",{"shows":shows})
+
+
