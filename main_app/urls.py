45e0d25dd410f21c977b3906d9ce20194fc3ede1
byilagan808 <b808ilagan@gmail.com>
2024-04-08 16:54:03 -1000

    pagination endpoint

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import index
 from .views import GetTvShows
+from .views import index,GetTvShows,GetTvShowsPagination
+
 
 urlpatterns = [
     path("", index),
     path('api/tv-shows/', GetTvShows.as_view(), name='Tv Shows get api endpoint'),
+    path('api/tv-shows/pagination/', GetTvShowsPagination.as_view(), name='Tv Shows pagination get api endpoint')
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from .models import *
 from rest_framework.views import APIView 
 from .serializers import * 
 from rest_framework.response import Response
+from .pagination import TvShowPagination
 
 
 # Create your views here.
@@ -15,5 +16,18 @@ class GetTvShows(APIView):
     def get(self, request): 
         queryset=TvShow.objects.prefetch_related("images").all() 
         serializer = TvShowSerializer(queryset,many=True,context={'request': request})
+        return Response(serializer.data)
+
+class GetTvShowsPagination(APIView): 
+    pagination_class = TvShowPagination 
+    def get(self, request, *args, **kwargs): 
+        queryset=TvShow.objects.prefetch_related("images").all() 
+        paginator = self.pagination_class()
+    
+        page = paginator.paginate_queryset(queryset, request) 
+        if page is not None: 
+            serializer = TvShowSerializer(page, many=True) 
+            return paginator.get_paginated_response(serializer.data) 
+        serializer = TvShowSerializer(queryset, many=True) 
         return Response(serializer.data)
 
